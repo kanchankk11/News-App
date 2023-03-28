a34068d763e9b1b6d7a522c9cbedc03e3771d106
kanchankk11 <nagkanchan5@gmail.com>
2023-03-28 18:48:56 +0530

    Source of news added

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Newsitem extends Component {
   render() {
-    let { title, desc, imgurl, newsurl } = this.props;
+    let { title, desc, imgurl, newsurl, author, publishedAt , source} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
@@ -10,14 +10,25 @@ export default class Newsitem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{desc}</p>
-            <a
-              href={newsurl}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-sm btn-primary"
-            >
-              Read more
-            </a>
+            <p className="card-text"><small className="text-muted">- {author?author : "Unknown"} on {new Date(publishedAt).toUTCString()}</small></p>
+            
+            <div className="d-flex justify-content-between">
+              <a
+                href={newsurl}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-sm btn-primary"
+              >
+                Read more
+              </a>
+
+              <div className="div">
+              <span className={source === "Independent" ? "badge bg-secondary" : "badge bg-info"}>{source}</span>
+              </div>
+              
+            </div>
+            
+            
           </div>
         </div>
       </div>
